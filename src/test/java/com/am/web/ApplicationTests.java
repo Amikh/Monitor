9ec49535e199f6d5fcca55d7c0f421ecd582e014
Amikh <amikh05@gmail.com>
2020-02-23 18:05:56 +0200

    change test package and class name

--- src/test/java/com/am/web/ApplicationTests.java
+++ src/test/java/com/am/web/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.in28minutes.springboot.web;
+package com.am.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootFirstWebApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
