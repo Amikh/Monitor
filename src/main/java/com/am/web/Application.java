864d5f838fbf4121b205cf543eafd3c85e7c01cd
Amikh <amikh05@gmail.com>
2020-02-23 18:03:31 +0200

    init

---
+++ src/main/java/com/am/web/Application.java
@@ -0,0 +1,14 @@
+package com.am.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.am.web")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
